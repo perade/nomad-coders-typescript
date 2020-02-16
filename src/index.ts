86e1d879b142a47a3b236881fb6ce9a4745fd89a
perade <perade0530@gmail.com>
2020-02-17 03:03:51 +0900

    4. Types in TypeScript 요약

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const name = "Hong",
+  age = 24,
+  gender = "male";
+
+const sayHi = (name: string, age: number, gender: string): void => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
